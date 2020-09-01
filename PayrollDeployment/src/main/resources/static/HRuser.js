880ea72d170800d54fece1c944514a267124eb30
amullarney <alasdar@sbcglobal.net>
2020-09-01 09:53:57 -0700

    Rudimentary UI
    Simplistic (untested) html + js for HRuser

--- PayrollDeployment/src/main/resources/static/HRuser.js
+++ PayrollDeployment/src/main/resources/static/HRuser.js
@@ -3,20 +3,36 @@ var stompClient = null;
 var vm = new Vue({
 	el: '#main-content',
 	data {
+	    PayrollRequestDisabled: true,
+	    UpdateDisabled: true,
+	    ApproveDisabled: true,
+	    SubmitDisabled: true,
 	    notification: ""
 	}
 })
+
+function initialize() {
+	    vm.PayrollRequestDisabled = true;
+	    vcm.UpdateDisabled = true;
+	    vm.ApproveDisabled: true,
+	    SubmitDisabled: true,
+	    notification = "";
+}
+
 
 function setConnected(connected) {
     $("#connect").prop("disabled", connected);
     $("#disconnect").prop("disabled", !connected);
     if (connected) {
         $("#conversation").show();
+        $("#payrollentries").hide();
     }
     else {
         $("#conversation").hide();
+        initialize();
     }
     $("#replies").html("");
+    $("#entries").html("");
 }
 
 // When connecting, subscribe to a topic to receive
@@ -27,7 +43,7 @@ function connect() {
     stompClient.connect({}, function (frame) {
         setConnected(true);
         console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/TestControl', function (reply) {
+        stompClient.subscribe('/topic/HRuser/', function (reply) {
             showReply(JSON.parse(reply.body).content);
         });
     });
@@ -51,9 +67,14 @@ function sendRetrievePayrollForReview() {
 function sendSubmitPayrollApproval() {
     stompClient.send("/app/SubmitPayrollForApproval", {}, 
       JSON.stringify({'department': $("#department").val()}));
+      vm.ApproveDisabled = true;
+      vm.SubmitDisabled = false;
+      $("#payrollentries").hide();
 }
 
-function sendUpdatesSent() {
+function sendUpdates() {
+    // send a SubmitItemHold message for any changed hold status
+    // then indicate all updates sent
     stompClient.send("/app/UpdatesSent", {}, 
       JSON.stringify({'department': $("#department").val()}));
 }
@@ -61,19 +82,32 @@ function sendUpdatesSent() {
 function sendSubmitToFinance() {
     stompClient.send("/app/SubmitToFinance", {}, 
       JSON.stringify({'department': $("#department").val()}));
+      vm.SubmitDisabled = true;
+}
+
+// Support functions for incoming message handling
+
+function payrollDataMsg( message ) {
+    // accept a payroll element data message - just display it, for now
+    $("#entries").append("<tr><td>" + message + "</td></tr>");
 }
 
 // Display a message received from the server.
+
 function showReply(message) {
     $("#replies").append("<tr><td>" + message + "</td></tr>");
     var messageName = JSON.parse( reply.body ).messageName;
     if ( messageName == "Notification" ) {
         vm.notification = JSON.parse( reply.body ).Message;
         if ( JSON.parse( reply.body ).Code == "UC05_b" ) {
-            $("#payroll").prop("disabled", False);
-
+           vm.PayrollDisabled = false;
+    } else if ( messageName == "PayrollData" ) {
+           vm.PayrollDisabled = true;
+           payrollDataMsg( message );
     } else if ( messageName == "PayrollSent" ) {
-           $("#update").prop("disabled", False);
+           vm.UpdateDisabled = false;
+           vm.ApproveDisabled = false;
+        $("#payrollentries").show();
 }
 
 // Map buttons to functions.
@@ -84,7 +118,7 @@ $(function () {
     $( "#connect" ).click(function() { connect(); });
     $( "#disconnect" ).click(function() { disconnect(); });
     $( "#payroll" ).click(function() { sendRetrievePayrollForReview(); });
-    $( "#update" ).click(function() { sendUpdatesSent(); });
+    $( "#update" ).click(function() { sendUpdates(); });
     $( "#approve" ).click(function() { sendSubmitPayrollApproval(); });
     $( "#submit" ).click(function() { sendSubmitToFinance(); });
 });
